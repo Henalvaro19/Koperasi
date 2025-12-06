ea6cd22f85276270be647b97ea718eb90b7c3c7e
Henalvaro19 <henalvarofarrel@gmail.com>
2025-12-06 12:21:17 +0700

    Update jam fix

--- hitung.js
+++ hitung.js
@@ -1,5 +1,5 @@
 // Format: new Date(tahun, bulan-1, tanggal, jam, menit, detik)
-const launchDate = new Date(2025, 11, 5, 17, 30, 0);
+const launchDate = new Date(2025, 11, 6, 17, 30, 0);
 localStorage.setItem("launchDate", launchDate.toISOString());
 const daysEl = document.getElementById('days');
 const hoursEl = document.getElementById('hours');
